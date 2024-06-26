8d2893769f64ec8ebf15227520599d22e6e03ed8
sureshstk <sureshstk4me@gmail.com>
2024-06-26 14:25:08 -0500

    Update ApiDemoApplication.java

--- src/main/java/com/example/api_demo/ApiDemoApplication.java
+++ src/main/java/com/example/api_demo/ApiDemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ApiDemoApplication {
 
-
+@GetMapping("/message")
 	public String message(){
 		return "Hello this os from Local Demo!!! for Azure";
 	}
